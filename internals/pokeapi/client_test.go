03e7f12fca84f21ba2cd5f7dd7ddf79aa09bdef8
pannipasra <new_nobita@hotmail.com>
2025-04-11 16:04:19 +0900

    Add empty test lol

---
+++ internals/pokeapi/client_test.go
@@ -0,0 +1,26 @@
+package pokeapi
+
+import "testing"
+
+// MockLocationAreaResponse creates a mock response for testing
+func createMockLocationAreaResponse(next, prev string) LocationAreaResp {
+	return LocationAreaResp{
+		Count:    1118,
+		Next:     &next,
+		Previous: &prev,
+		Results: []LocationAreaResult{
+			{
+				Name: "canalave-city-area",
+				URL:  "https://pokeapi.co/api/v2/location-area/1/",
+			},
+			{
+				Name: "eterna-city-area",
+				URL:  "https://pokeapi.co/api/v2/location-area/2/",
+			},
+		},
+	}
+}
+
+func TestListLocationAreas(t *testing.T) {
+
+}
